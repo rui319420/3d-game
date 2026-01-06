ebbac20c9469080e194f25dbedc2ca9425cd6cf8
Rui <s1F102501866@iniad.org>
2026-01-06 22:11:43 +0900

    feat: 重力を実装した

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,36 @@
 import { Engine, Scene, FreeCamera, Vector3, HemisphericLight, MeshBuilder, 
-  HavokPlugin, PhysicsAggregate, PhysicsShapeType
+  HavokPlugin, PhysicsAggregate, PhysicsShapeType,
 } from "@babylonjs/core";
 import HavokPhysics from "@babylonjs/havok";
 
-const canvas = document.getElementById("renderCanvas") as HTMLCanvasElement;
+async function createGame() {
+  const canvas = document.getElementById("renderCanvas") as HTMLCanvasElement;
+  canvas.style.width = "100%";
+  canvas.style.height = "100%";
+  const engine = new Engine(canvas, true);
+  const scene = new Scene(engine);
 
-const engine = new Engine(canvas, true);
+  const havokInstance = await HavokPhysics({
+      locateFile: () => "./HavokPhysics.wasm"
+  });
+  const havokPlugin = new HavokPlugin(true, havokInstance);
+  scene.enablePhysics(new Vector3(0, -9.8, 0), havokPlugin);
 
-const scene = new Scene(engine);
+  const camera = new FreeCamera("camera1", new Vector3(0, 5, -10), scene);
+  camera.setTarget(Vector3.Zero());
+  const light = new HemisphericLight("light1", new Vector3(0, 1, 0), scene);
 
-const camera = new FreeCamera("camera1", new Vector3(0, 5, -10), scene);
-camera.setTarget(Vector3.Zero());
+  const sphere = MeshBuilder.CreateSphere("sphere", { diameter: 2 }, scene);
+  sphere.position.y = 5;
 
-const light = new HemisphericLight("light1", new Vector3(0, 1, 0), scene);
+  new PhysicsAggregate(sphere, PhysicsShapeType.SPHERE, { mass: 1 }, scene);
+  const ground = MeshBuilder.CreateGround("ground", { width: 10, height: 10 }, scene);
 
-const sphere = MeshBuilder.CreateSphere("sphere", { diameter: 2 }, scene);
-sphere.position.y = 3;
+  new PhysicsAggregate(ground, PhysicsShapeType.BOX, { mass: 0 }, scene);
 
-const ground = MeshBuilder.CreateGround("ground", { width: 10, height: 10 }, scene);
+  engine.runRenderLoop(() => {
+      scene.render();
+  });
+}
 
-engine.runRenderLoop(() => {
-    scene.render();
-});
+createGame();
