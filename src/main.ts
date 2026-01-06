77c5e11ec038878eb576734bbe49e79c0a788a13
Rui <s1F102501866@iniad.org>
2026-01-06 21:19:08 +0900

    feat: ボールと地面を追加

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,7 @@
-import { Engine, Scene, FreeCamera, Vector3, HemisphericLight } from "@babylonjs/core";
+import { Engine, Scene, FreeCamera, Vector3, HemisphericLight, MeshBuilder, 
+  HavokPlugin, PhysicsAggregate, PhysicsShapeType
+} from "@babylonjs/core";
+import HavokPhysics from "@babylonjs/havok";
 
 const canvas = document.getElementById("renderCanvas") as HTMLCanvasElement;
 
@@ -10,6 +13,11 @@ const camera = new FreeCamera("camera1", new Vector3(0, 5, -10), scene);
 camera.setTarget(Vector3.Zero());
 
 const light = new HemisphericLight("light1", new Vector3(0, 1, 0), scene);
+
+const sphere = MeshBuilder.CreateSphere("sphere", { diameter: 2 }, scene);
+sphere.position.y = 3;
+
+const ground = MeshBuilder.CreateGround("ground", { width: 10, height: 10 }, scene);
 
 engine.runRenderLoop(() => {
     scene.render();
