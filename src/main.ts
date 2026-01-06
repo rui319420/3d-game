328fc30f767272f939713c2dd6aa71ab21f38fee
Rui <s1F102501866@iniad.org>
2026-01-06 23:39:12 +0900

    feat: 矢印キーでボールが移動するようにした

--- src/main.ts
+++ src/main.ts
@@ -5,8 +5,20 @@ import HavokPhysics from "@babylonjs/havok";
 
 async function createGame() {
   const canvas = document.getElementById("renderCanvas") as HTMLCanvasElement;
+
+  document.body.style.margin = "0";
+  document.body.style.overflow = "hidden";
   canvas.style.width = "100%";
   canvas.style.height = "100%";
+
+  const inputMap = {};
+  window.addEventListener("keydown", (ev) => {
+    inputMap[ev.key] = true;
+  })
+  window.addEventListener("keyup", (ev) => {
+    inputMap[ev.key] = false;
+  })
+
   const engine = new Engine(canvas, true);
   const scene = new Scene(engine);
 
@@ -16,20 +28,43 @@ async function createGame() {
   const havokPlugin = new HavokPlugin(true, havokInstance);
   scene.enablePhysics(new Vector3(0, -9.8, 0), havokPlugin);
 
-  const camera = new FreeCamera("camera1", new Vector3(0, 5, -10), scene);
+  const camera = new FreeCamera("camera1", new Vector3(0, 10, -10), scene);
   camera.setTarget(Vector3.Zero());
+  camera.attachControl(canvas, true);
+
   const light = new HemisphericLight("light1", new Vector3(0, 1, 0), scene);
 
   const sphere = MeshBuilder.CreateSphere("sphere", { diameter: 2 }, scene);
   sphere.position.y = 5;
 
-  new PhysicsAggregate(sphere, PhysicsShapeType.SPHERE, { mass: 1 }, scene);
   const ground = MeshBuilder.CreateGround("ground", { width: 10, height: 10 }, scene);
-
   new PhysicsAggregate(ground, PhysicsShapeType.BOX, { mass: 0 }, scene);
+  const sphereAggregate = new PhysicsAggregate(sphere, PhysicsShapeType.SPHERE, { mass: 1, restitution: 0.7 }, scene);
+
+  scene.onBeforeRenderObservable.add(() => {
+    const body = sphereAggregate.body;
+
+    const power = 0.5;
+
+    if (inputMap["ArrowUp"]) {
+      body.applyImpulse(new Vector3(0, 0, power), sphere.getAbsolutePosition());
+    }
+    if (inputMap["ArrowDown"]) {
+            body.applyImpulse(new Vector3(0, 0, -power), sphere.getAbsolutePosition());
+    }
+    if (inputMap["ArrowLeft"]) { 
+            body.applyImpulse(new Vector3(-power, 0, 0), sphere.getAbsolutePosition());
+    }
+    if (inputMap["ArrowRight"]) {
+            body.applyImpulse(new Vector3(power, 0, 0), sphere.getAbsolutePosition());
+    }
+  })
 
   engine.runRenderLoop(() => {
       scene.render();
+  });
+  window.addEventListener("resize", () => {
+      engine.resize();
   });
 }
 
